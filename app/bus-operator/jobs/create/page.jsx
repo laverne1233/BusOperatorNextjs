aebb7b861422f3a4ca8f708b321d6197a191a132
June Vic Cadayona <jvcadz@gmail.com>
2024-01-15 07:48:02 +0800

    add initial job create functionality

--- app/bus-operator/jobs/create/page.jsx
+++ app/bus-operator/jobs/create/page.jsx
@@ -12,6 +12,7 @@ export default function JobCreate() {
     const jobStore = useJobStore()
     const userStore = useUserStore()
     const [organization, setOrganization] = useState({})
+    const [questions, setQuestions] = useState([])
 
     useEffect(() => {
         jobStore.reset()
@@ -36,17 +37,44 @@ export default function JobCreate() {
         jobStore.setJob({ [e.target.name]: e.target.value })
     }
 
+    const handleQuestionOnChangeEvent = (event, index) => {
+        let { name, value } = event.target;
+        let onChangeValue = [...questions];
+        onChangeValue[index]['value'] = value;
+        setQuestions(onChangeValue);
+    }
+
+    const handleAddQuestion = () => {
+        setQuestions([...questions, { name: `field${questions.length + 1}` }]);
+    }
+
+    const handleSave = async () => {
+        const response = await fetch(process.env.BE_URL + '/jobs', {
+            headers: {
+                Authorization: `Bearer ${userStore.user?.token}`,
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': '*',
+                'Access-Control-Allow-Headers': 'Content-Type',
+            },
+            method: 'POST',
+            body: JSON.stringify({ ...jobStore.job, 'questions': questions.map(item => ({ 'description': item.value })).filter(item => item.description) })
+        })
+
+        const result = await response.json()
+    }
+
     return (
         <>
             <div className="flex flex-row w-full">
                 <Heading title={'Manage Jobs'} description={'Create new job opening'} />
                 <div className="ml-auto flex flex-row p-8 gap-4">
                     <Link href={redirectToRoute('/jobs')} className="bg-white-500 text-black border border-black rounded-lg py-4 px-8  w-full">Cancel</Link>
-                    <button className="bg-purple-default text-white border border-white rounded-lg py-1 px-10  w-full">Save</button>
+                    <button className="bg-purple-default text-white border border-white rounded-lg py-1 px-10  w-full" onClick={handleSave}>Save</button>
                 </div>
             </div>
             <TopForm companyName={organization.company_name} companyAddress={organization.company_address} onChangeEvent={handleChangeEvent} />
-            <Tabs companyDescription={organization.company_description} onChangeEvent={handleChangeEvent} />
+            <Tabs companyDescription={organization.company_description} onChangeEvent={handleChangeEvent} questionOnChangeEvent={handleQuestionOnChangeEvent} questions={questions} handleAddQuestion={handleAddQuestion} />
         </>
     )
 }
@@ -103,7 +131,7 @@ const TopForm = ({ companyName, companyAddress, onChangeEvent }) => {
     )
 }
 
-const Tabs = ({ companyDescription, onChangeEvent }) => {
+const Tabs = ({ companyDescription, onChangeEvent, questions, questionOnChangeEvent, handleAddQuestion }) => {
     return (
         <>
             <div className="border-b border-gray-200 dark:border-gray-700">
@@ -151,14 +179,30 @@ const Tabs = ({ companyDescription, onChangeEvent }) => {
                     />
                 </div>
                 <div id="horizontal-alignment-3" className="hidden" role="tabpanel" aria-labelledby="horizontal-alignment-item-3">
-                    <p className="text-gray-500 dark:text-gray-400">
-                        Coming soon.
-                    </p>
+                    <div className="flex flex-col gap-4">
+                        {questions.map((item, index) => (<QuestionField index={index} key={index} tagFor={item.name} onchangeEvent={questionOnChangeEvent} />))}
+                        <button onClick={handleAddQuestion} className="bg-purple-default text-white border border-white rounded-lg py-4 px-10  w-full">Add Question</button>
+                    </div>
                 </div>
             </div>
         </>
     )
 }
 
-
-
+const QuestionField = ({ index, tagFor, placeholder, inputClass, defaultValue, rest, onchangeEvent }) => {
+    return (
+        <div className="flex flex-row gap-3 justify-center items-center">
+            {index + 1}
+            <input
+                type={'text'}
+                name={tagFor}
+                id={tagFor}
+                className={inputClass || "py-3 px-4 w-full block w-3/4 border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none"}
+                placeholder={placeholder}
+                onChange={(event) => onchangeEvent(event, index)}
+                defaultValue={defaultValue}
+                {...rest}
+            />
+        </div>
+    )
+}
